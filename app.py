a6cddb9677508a631d121d4a9941a1003634faca
olasubomiajibare83-code <olasubomiajibare83@gmail.com>
2026-08-24 17:44:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import pipeline
 
-chatbot = pipeline("text-generation", model="microsoft/DialoGPT-medium")
+chatbot = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta")
 
 st.title("Kingsbot")
 if "messages" not in st.session_state:
@@ -15,7 +15,7 @@ if prompt := st.chat_input("Ask me anything"):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-    response = chatbot(prompt, max_length=150)[0]['generated_text']
+    response = chatbot(prompt, max_new_tokens=200)[0]['generated_text']
     
     with st.chat_message("assistant"):
         st.markdown(response)
